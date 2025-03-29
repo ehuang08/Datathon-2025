2815b7d87350d5244786b26f36a3efa9d82d0098
carolinef35 <cf35@illinois.edu>
2025-03-29 13:35:24 -0500

    prophet model for predicting q4

---
+++ Prophet Model.R
@@ -0,0 +1,30 @@
+install.packages("devtools")
+devtools::install_github("FinYang/tsdl")
+install.packages("prophet")
+library(tidyverse)
+library(tsdl)
+library(prophet)
+library(dplyr)
+
+transactions <- read_csv("cleaned_transaction.csv")
+
+str(transactions)
+transactions$month <- as.Date(paste(transactions$month, "01", sep = "-"), format = "%Y-%m-%d")
+class(transactions$month)
+str(transactions)
+print(transactions$month)
+
+transactions_filtered <- transactions %>%
+  filter(transactions$month >= as.Date("2024-07-01"))
+
+transactions_filtered <- mutate(
+  transactions_filtered, 
+  ds = transactions_filtered$month,
+  y = transactions_filtered$total_transaction,
+)
+
+m <- prophet(transactions_filtered)
+future <- make_future_dataframe(m, periods = 304)
+forecast <- predict(m, future)
+plot(m, forecast)
+prophet_plot_components(m, forecast)
